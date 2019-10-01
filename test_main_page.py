96e5ace074443848c3d3b50c610daf9df645ffad
EugenieKurkova <eugeniepoddubitskaya@gmail.com>
2019-10-01 18:56:10 +0300

    added init file

--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,18 @@
 from .page.main_page import MainPage
-
-
-def test_guest_can_go_to_login_page(parametrized_browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(parametrized_browser, url=link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()
+from .page.login_page import LoginPage
 
 
 def test_guest_should_see_login_link(parametrized_browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(parametrized_browser, url=link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page(parametrized_browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(parametrized_browser, url=link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(parametrized_browser, parametrized_browser.current_url)
+    login_page.should_be_login_page()
